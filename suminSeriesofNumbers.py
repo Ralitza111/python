79185c980d38360344d342c2504fe7fbd292d15a
Ralitza111 <37781062+Ralitza111@users.noreply.github.com>
2019-12-29 12:17:29 -0800

    Delete suminSeriesofNumbers.py

--- suminSeriesofNumbers.py
+++
@@ -1,8 +0,0 @@
-SeriesofNumbers=raw_input(" Enter numbers separated by space ")
-sum=0
-for numbers in SeriesofNumbers:
-    numbers=numbers.split()
-    for n in numbers:
-        n=int(n)
-        sum=sum + n
-print("The sum is",sum)
